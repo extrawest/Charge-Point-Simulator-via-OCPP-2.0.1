9602b2f72b84adb5e4aceb91eb07135ddb4d10f7
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-11-18 10:35:42 +0200

    initial commit

---
+++ src/main/java/com/extrawest/ocpp/stationemulation/OcppStationEmulationApplication.java
@@ -0,0 +1,13 @@
+package com.extrawest.ocpp.stationemulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppStationEmulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcppStationEmulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/extrawest/ocpp/stationemulation/OcppStationEmulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extrawest.ocpp.stationemulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcppStationEmulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
