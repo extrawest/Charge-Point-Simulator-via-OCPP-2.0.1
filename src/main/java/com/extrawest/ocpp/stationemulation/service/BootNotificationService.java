ffa651c220c9479052d6a30ea83ef766278f0ed2
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-11-22 21:35:05 +0200

    [-] - redundant code was removed

--- src/main/java/com/extrawest/ocpp/stationemulation/service/BootNotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.extrawest.ocpp.stationemulation.service;
-
-import java.util.concurrent.ExecutionException;
-
-public interface BootNotificationService {
-    void setClientSession() throws ExecutionException, InterruptedException;
-}
