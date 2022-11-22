6e2cdb61f6de786c8fd3f051d66c35d5a6ac75f1
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-11-22 22:48:38 +0200

    [~] - internal classes were extracted to separate files

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/configuration/EuChargetimeConfiguration.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/configuration/EuChargetimeConfiguration.java
@@ -4,7 +4,6 @@ import eu.chargetime.ocpp.ClientEvents;
 import eu.chargetime.ocpp.JSONClient;
 import eu.chargetime.ocpp.feature.profile.ClientCoreEventHandler;
 import eu.chargetime.ocpp.feature.profile.ClientCoreProfile;
-import eu.chargetime.ocpp.model.core.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -16,21 +15,6 @@ import org.springframework.context.annotation.Configuration;
 public class EuChargetimeConfiguration {
 
     @Bean
-    public ClientEvents clientEvents() {
-        return new ClientEvents() {
-            @Override
-            public void connectionOpened() {
-                log.info("ClientEvents - connectionOpened()");
-            }
-
-            @Override
-            public void connectionClosed() {
-                log.info("ClientEvents - connectionClosed()");
-            }
-        };
-    }
-
-    @Bean
     public JSONClient jsonClient(ClientCoreProfile coreProfile) {
         return new JSONClient(coreProfile);
     }
@@ -38,69 +22,6 @@ public class EuChargetimeConfiguration {
     @Bean
     public ClientCoreProfile coreProfile(ClientCoreEventHandler clientCoreEventHandler) {
         return new ClientCoreProfile(clientCoreEventHandler);
-    }
-
-    @Bean
-    public ClientCoreEventHandler clientCoreEventHandler() {
-        return new ClientCoreEventHandler() {
-            @Override
-            public ChangeAvailabilityConfirmation handleChangeAvailabilityRequest(ChangeAvailabilityRequest request) {
-                log.debug("ChangeAvailabilityRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public GetConfigurationConfirmation handleGetConfigurationRequest(GetConfigurationRequest request) {
-                log.debug("GetConfigurationRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public ChangeConfigurationConfirmation handleChangeConfigurationRequest(ChangeConfigurationRequest request) {
-                log.debug("ChangeConfigurationRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public ClearCacheConfirmation handleClearCacheRequest(ClearCacheRequest request) {
-                log.debug("ClearCacheRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public DataTransferConfirmation handleDataTransferRequest(DataTransferRequest request) {
-                log.debug("DataTransferRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public RemoteStartTransactionConfirmation handleRemoteStartTransactionRequest(RemoteStartTransactionRequest request) {
-                log.debug("RemoteStartTransactionRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public RemoteStopTransactionConfirmation handleRemoteStopTransactionRequest(RemoteStopTransactionRequest request) {
-                log.debug("RemoteStopTransactionRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public ResetConfirmation handleResetRequest(ResetRequest request) {
-                log.debug("ResetRequest: " + request);
-                return unsupported();
-            }
-
-            @Override
-            public UnlockConnectorConfirmation handleUnlockConnectorRequest(UnlockConnectorRequest request) {
-                log.debug("UnlockConnectorRequest: " + request);
-                return unsupported();
-            }
-
-            private <T> T unsupported() {
-                return null;  // returning null means unsupported feature
-            }
-        };
     }
 
     @Bean
