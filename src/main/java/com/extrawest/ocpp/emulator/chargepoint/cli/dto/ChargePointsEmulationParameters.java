4a9fd1457f8df3b4783d92627260092673551ffb
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-11-25 15:46:23 +0200

    [~] - minor code style enhancements

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/dto/ChargePointsEmulationParameters.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/dto/ChargePointsEmulationParameters.java
@@ -1,18 +1,16 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
-@Builder
 @Getter
 public class ChargePointsEmulationParameters {
 
-    @Min(1)
-    private long chargePointsAmount;
-
     @NotBlank
     private String centralSystemUrl;
+
+    @Min(1)
+    private long chargePointsCount;
 }
