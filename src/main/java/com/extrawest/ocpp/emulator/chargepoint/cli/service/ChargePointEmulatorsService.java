b90a54906a985cc32aff42b8e984920cc2b05092
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-11-25 20:29:45 +0200

    Revert "[~] - @AllArgsConstructor was replaced with @RequiredArgsConstructor"
    This reverts commit c2807b805e1be60282429c30f8c99dea35315bf9.

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/service/ChargePointEmulatorsService.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/service/ChargePointEmulatorsService.java
@@ -2,9 +2,7 @@ package com.extrawest.ocpp.emulator.chargepoint.cli.service;
 
 import com.extrawest.ocpp.emulator.chargepoint.cli.dto.ChargePointsEmulationParameters;
 
-import javax.validation.Valid;
-
 public interface ChargePointEmulatorsService {
 
-    void startEmulation(@Valid ChargePointsEmulationParameters parameters);
+    void startEmulation(ChargePointsEmulationParameters parameters);
 }
