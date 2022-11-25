4a956ab4e3ed2f0a7fe6de857893ab3f2881dca6
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-11-25 20:30:38 +0200

    [~] - @AllArgsConstructor was replaced with @RequiredArgsConstructor

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/dto/ChargePointsEmulationParameters.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/dto/ChargePointsEmulationParameters.java
@@ -1,18 +1,18 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ChargePointsEmulationParameters {
 
     @NotBlank
-    private String centralSystemUrl;
+    private final String centralSystemUrl;
 
     @Min(1)
-    private long chargePointsCount;
+    private final long chargePointsCount;
 }
