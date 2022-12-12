16f9ebad3cfa5f3115003c6e9a5af015f83ce398
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-12 02:31:34 +0200

    [~] - a JSON date format was unhardcoded to ModelConstants

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/constant/ModelConstants.java
@@ -0,0 +1,10 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ModelConstants {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+}
