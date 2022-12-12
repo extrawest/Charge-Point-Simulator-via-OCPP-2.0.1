1bd7ff422dfa4239858f7c66e26091147b8df912
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-12 02:23:49 +0200

    [-] - eu.chargetime classes were removed

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/euchargetime/IClientAPIFactory.java
+++
@@ -1,8 +0,0 @@
-package com.extrawest.ocpp.emulator.chargepoint.cli.euchargetime;
-
-import eu.chargetime.ocpp.IClientAPI;
-
-public interface IClientAPIFactory {
-
-    IClientAPI createNewClientApi();
-}
