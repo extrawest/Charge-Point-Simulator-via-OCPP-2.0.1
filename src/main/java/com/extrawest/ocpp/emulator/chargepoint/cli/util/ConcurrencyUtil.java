fe051a65bc8278d79d1b9ba255c98fa675fc2ee1
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-16 12:50:41 +0200

    [+] - ConcurrencyUtil was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/util/ConcurrencyUtil.java
@@ -0,0 +1,35 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.Supplier;
+
+import static com.extrawest.ocpp.emulator.chargepoint.cli.util.ThrowReadablyUtil.unchecked;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
+public class ConcurrencyUtil {
+
+    public static <T> T acquireGetRelease(Semaphore semaphore, Supplier<T> supplier) {
+        try {
+            semaphore.acquire(1);
+            return supplier.get();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+            throw unchecked(e);
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    public static void acquireRunRelease(Semaphore semaphore, Runnable runnable) {
+        acquireGetRelease(semaphore, () -> {
+            runnable.run();
+            return (Void) null;
+        });
+    }
+}
