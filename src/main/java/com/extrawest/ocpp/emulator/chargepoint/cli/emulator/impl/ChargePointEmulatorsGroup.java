a2b8db4fc219f08928d3abeb9952c15e6d7519f1
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-12-20 06:41:45 +0200

    added new field in ChargePointEmulatorsGroup.java

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/impl/ChargePointEmulatorsGroup.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/impl/ChargePointEmulatorsGroup.java
@@ -1,26 +1,42 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.emulator.impl;
 
+import com.extrawest.ocpp.emulator.chargepoint.cli.constant.PicocliConstants;
 import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
 import com.extrawest.ocpp.emulator.chargepoint.cli.exception.emulator.EmulationException;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Slf4j
 @RequiredArgsConstructor
+@Setter
 public class ChargePointEmulatorsGroup implements ChargePointEmulator {
 
     private final Collection<ChargePointEmulator> emulators;
+    @Value("${ocpp.charge-point.logs-count}")
+    private int logsCount;
 
     @Override
     public void start() throws EmulationException {
         var startedEmulators = new ArrayList<ChargePointEmulator>(emulators.size());
+        log.info("Starting " + emulators.size() + " emulators");
         try {
             for (var chargePointEmulator : emulators) {
                 chargePointEmulator.start();
                 startedEmulators.add(chargePointEmulator);
+                if (logsCount != 0) {
+                    if (startedEmulators.size() % logsCount == 0) {
+                        log.info("Currently running " + startedEmulators.size() + " charge points emulators" );
+                    }
+                } else if (startedEmulators.size() % PicocliConstants.DEFAULT_LOGS_RANGE == 0) {
+                    log.info("Currently running " + startedEmulators.size() + " charge points emulators" );
+                }
+
             }
         } catch (EmulationException e) {
             log.error(e.getMessage(), e);
@@ -35,6 +51,8 @@ public class ChargePointEmulatorsGroup implements ChargePointEmulator {
     }
 
     private void stopChargePointEmulators(Collection<ChargePointEmulator> chargePointEmulators) {
+        log.info("Stopping " + chargePointEmulators.size() + " charge points emulators");
         chargePointEmulators.forEach(ChargePointEmulator::stop);
+        log.info("All charge points emulators was stopped");
     }
 }
