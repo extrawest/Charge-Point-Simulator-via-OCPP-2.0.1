b148c6fd793c584db6e7a2b24c738f0e900a3c2d
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-25 20:04:43 +0200

    [+] - ThrowingRunnable was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/util/ThrowingRunnable.java
@@ -0,0 +1,18 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.util;
+
+import static com.extrawest.ocpp.emulator.chargepoint.cli.util.ThrowReadablyUtil.unchecked;
+
+@FunctionalInterface
+public interface ThrowingRunnable extends Runnable {
+
+    @Override
+    default void run() {
+        try {
+            tryRun();
+        } catch (Exception e) {
+            throw unchecked(e);
+        }
+    }
+
+    void tryRun() throws Exception;
+}
