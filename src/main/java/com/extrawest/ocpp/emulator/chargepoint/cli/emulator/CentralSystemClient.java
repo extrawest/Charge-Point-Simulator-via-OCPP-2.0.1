c08be697d099567c42a1e03bdc9eba7ff0b734e1
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-25 20:04:06 +0200

    [~] - CentralSystemClient was refactored to throw EmulationIOException

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/CentralSystemClient.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/CentralSystemClient.java
@@ -1,6 +1,7 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.emulator;
 
 import com.extrawest.ocpp.emulator.chargepoint.cli.exception.emulator.EmulationConnectionException;
+import com.extrawest.ocpp.emulator.chargepoint.cli.exception.emulator.EmulationIOException;
 import com.extrawest.ocpp.emulator.chargepoint.cli.model.call.Call;
 import com.extrawest.ocpp.emulator.chargepoint.cli.model.call.CallResult;
 
@@ -12,5 +13,5 @@ public interface CentralSystemClient {
 
     void disconnect();
 
-    <T, U> CallResult<U> sendCall(Call<T> call, Class<U> expectedCallResultClass);
+    <T, U> CallResult<U> sendCall(Call<T> call, Class<U> expectedCallResultClass) throws EmulationIOException;
 }
