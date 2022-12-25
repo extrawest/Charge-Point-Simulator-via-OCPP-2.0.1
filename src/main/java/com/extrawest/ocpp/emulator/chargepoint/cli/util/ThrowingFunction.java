700c64c7062a36cdebd7672f5b57f2505fffc36b
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-25 20:04:36 +0200

    [+] - ThrowingFunction was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/util/ThrowingFunction.java
@@ -0,0 +1,20 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.util;
+
+import java.util.function.Function;
+
+import static com.extrawest.ocpp.emulator.chargepoint.cli.util.ThrowReadablyUtil.unchecked;
+
+@FunctionalInterface
+public interface ThrowingFunction<T, R> extends Function<T, R> {
+
+    @Override
+    default R apply(T argument) {
+        try {
+            return tryApply(argument);
+        } catch (Exception e) {
+            throw unchecked(e);
+        }
+    }
+
+    R tryApply(T argument) throws Exception;
+}
