8542871e791fea2238acda3b52f1a8bf090b6345
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-26 13:17:55 +0200

    [~] - createCallFor(request: AuthorizeRequest) was added

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/CallFactory.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/CallFactory.java
@@ -1,5 +1,6 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model.call;
 
+import com.extrawest.ocpp.emulator.chargepoint.cli.model.AuthorizeRequest;
 import com.extrawest.ocpp.emulator.chargepoint.cli.model.BootNotificationRequest;
 import com.extrawest.ocpp.emulator.chargepoint.cli.model.HeartbeatRequest;
 
@@ -8,4 +9,6 @@ public interface CallFactory {
     Call<BootNotificationRequest> createCallFor(BootNotificationRequest bootNotificationRequest);
 
     Call<HeartbeatRequest> createCallFor(HeartbeatRequest heartbeatRequest);
+
+    Call<AuthorizeRequest> createCallFor(AuthorizeRequest authorizeRequest);
 }
