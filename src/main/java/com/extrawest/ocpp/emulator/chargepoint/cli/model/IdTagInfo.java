f9a149b3d10461aeabcca728e749c0a42c8b1e3e
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-26 13:15:33 +0200

    [+] - IdTagInfo was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/IdTagInfo.java
@@ -0,0 +1,26 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor(onConstructor = @__(@JsonCreator))
+@Getter
+@ToString
+public class IdTagInfo {
+
+    @JsonProperty("expiryDate")
+    private final LocalDateTime expiryDate;
+
+    @JsonProperty("parentIdTag")
+    private final IdToken parentIdTag;
+
+    @JsonProperty("status")
+    @NonNull
+    private final AuthorizationStatus status;
+}
