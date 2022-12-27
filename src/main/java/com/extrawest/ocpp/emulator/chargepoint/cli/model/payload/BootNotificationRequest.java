41a6d700eafac77fad0eed6bb616623760349a32
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-27 10:59:18 +0200

    [~] - the type of chargePointModel and chargePointVendor was changed from String to CiString20

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/BootNotificationRequest.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/BootNotificationRequest.java
@@ -1,13 +1,14 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model.payload;
 
+import com.extrawest.ocpp.emulator.chargepoint.cli.model.CiString20;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class BootNotificationRequest { // TODO: refactor to use CiStirng20
+public class BootNotificationRequest {
 
-    private final String chargePointModel;
+    private final CiString20 chargePointModel;
 
-    private final String chargePointVendor;
+    private final CiString20 chargePointVendor;
 }
