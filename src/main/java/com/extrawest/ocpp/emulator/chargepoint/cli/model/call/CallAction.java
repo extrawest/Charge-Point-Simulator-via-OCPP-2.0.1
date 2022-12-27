019d25b5d4c28c6f16f35ac1a8641d2be4f0f0a6
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-27 10:32:33 +0200

    [+] - MeterValues was added

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/CallAction.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/CallAction.java
@@ -2,5 +2,5 @@ package com.extrawest.ocpp.emulator.chargepoint.cli.model.call;
 
 public enum CallAction {
 
-    Authorize, BootNotification, Heartbeat, StartTransaction
+    Authorize, BootNotification, Heartbeat, StartTransaction, MeterValues
 }
