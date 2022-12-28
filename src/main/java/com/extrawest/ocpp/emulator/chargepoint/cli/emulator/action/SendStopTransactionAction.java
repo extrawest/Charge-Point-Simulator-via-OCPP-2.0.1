a261b49503e60f029eabc4286e8e94c6b2af65ee
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-28 21:58:15 +0200

    [+] - SendStopTransactionAction was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/action/SendStopTransactionAction.java
@@ -0,0 +1,42 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.emulator.action;
+
+import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
+import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.RequestSender;
+import com.extrawest.ocpp.emulator.chargepoint.cli.exception.emulator.EmulationIOException;
+import com.extrawest.ocpp.emulator.chargepoint.cli.model.payload.StopTransactionRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.extrawest.ocpp.emulator.chargepoint.cli.util.ThrowReadablyUtil.transactionNotStarted;
+import static com.extrawest.ocpp.emulator.chargepoint.cli.util.ThrowReadablyUtil.unchecked;
+
+@Component
+@RequiredArgsConstructor
+public class SendStopTransactionAction implements Consumer<ChargePointEmulator> {
+
+    private final RequestSender requestSender;
+
+    @Override
+    public void accept(ChargePointEmulator chargePointEmulator) {
+        var request = createRequestFor(chargePointEmulator);
+        var client = chargePointEmulator.getCentralSystemClient();
+        try {
+            requestSender.sendRequest(client, request);
+        } catch (EmulationIOException e) {
+            throw unchecked(e);
+        }
+    }
+
+    private StopTransactionRequest createRequestFor(ChargePointEmulator chargePointEmulator) {
+        return new StopTransactionRequest(
+            chargePointEmulator.getCurrentMeterValue(),
+            LocalDateTime.now(),
+            Optional.of(chargePointEmulator.getCurrentTransactionId())
+                .orElseThrow(() -> transactionNotStarted(chargePointEmulator))
+        );
+    }
+}
