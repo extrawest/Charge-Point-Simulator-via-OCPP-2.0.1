4040fa1746697bfc60d47b5e177da1834e57aa7c
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-28 21:55:48 +0200

    [+] - factory-functions for common IllegalStateApplicationException cases were added

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/util/ThrowReadablyUtil.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/util/ThrowReadablyUtil.java
@@ -1,5 +1,7 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.util;
 
+import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
+import com.extrawest.ocpp.emulator.chargepoint.cli.exception.IllegalStateApplicationException;
 import com.extrawest.ocpp.emulator.chargepoint.cli.exception.UncheckedApplicationException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -17,5 +19,17 @@ public class ThrowReadablyUtil {
 
     public static UncheckedApplicationException emptyOptionalException() {
         return new UncheckedApplicationException("The Optional was expected to be non-empty");
+    }
+
+    public static IllegalStateApplicationException transactionNotStarted(ChargePointEmulator chargePointEmulator) {
+        return new IllegalStateApplicationException(
+            "There is no transaction associated with the emulator^ " + chargePointEmulator
+        );
+    }
+
+    public static IllegalStateApplicationException heartbeatNotSet(ChargePointEmulator chargePointEmulator) {
+        return new IllegalStateApplicationException(
+            "There is no heartbeat set for the emulator: " + chargePointEmulator
+        );
     }
 }
