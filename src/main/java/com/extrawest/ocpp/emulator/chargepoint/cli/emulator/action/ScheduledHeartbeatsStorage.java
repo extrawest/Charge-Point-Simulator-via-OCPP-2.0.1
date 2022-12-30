1b8115724a8a4b7a3013b0b8a22bf6ea52950803
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-30 22:19:23 +0200

    [+] - ScheduledHeartbeatsStorage was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/action/ScheduledHeartbeatsStorage.java
@@ -0,0 +1,33 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.emulator.action;
+
+import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
+import com.extrawest.ocpp.emulator.chargepoint.cli.exception.IllegalStateApplicationException;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ScheduledFuture;
+
+@Component
+class ScheduledHeartbeatsStorage {
+
+    private final Map<ChargePointEmulator, ScheduledFuture<?>> scheduledHeartbeats = new HashMap<>();
+
+    public void store(ChargePointEmulator chargePointEmulator, ScheduledFuture<?> heartbeatingFuture) {
+        if (scheduledHeartbeats.containsKey(chargePointEmulator)) {
+            throw new IllegalStateApplicationException(
+                "The storage already contains a scheduled heartbeating future for the emulator"
+            );
+        }
+        scheduledHeartbeats.put(chargePointEmulator, heartbeatingFuture);
+    }
+
+    public ScheduledFuture<?> remove(ChargePointEmulator chargePointEmulator) {
+        return Optional.of(chargePointEmulator)
+            .map(scheduledHeartbeats::get)
+            .orElseThrow(() -> new IllegalStateApplicationException(
+                "There is no heartbeating scheduled future in this storage. Consider storing"
+            ));
+    }
+}
