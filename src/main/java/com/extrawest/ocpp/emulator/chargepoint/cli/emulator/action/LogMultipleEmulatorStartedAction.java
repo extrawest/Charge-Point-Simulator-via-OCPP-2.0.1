2d4d080d19a04939084f20e1290f09b1ac03edbb
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2022-12-30 21:39:53 +0200

    [+] - LogMultipleEmulatorStartedAction was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/action/LogMultipleEmulatorStartedAction.java
@@ -0,0 +1,29 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.emulator.action;
+
+import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+@Slf4j
+public class LogMultipleEmulatorStartedAction implements Consumer<ChargePointEmulator> {
+
+    private final int logMultipleForLogs;
+
+    private int currentLogRequestCounter;
+
+    @Override
+    public void accept(ChargePointEmulator chargePointEmulator) {
+        currentLogRequestCounter++;
+
+        if (indexNeedsBeLogged(currentLogRequestCounter, logMultipleForLogs)) {
+            log.info("Currently running " + currentLogRequestCounter + " charge points emulators");
+        }
+    }
+
+    private boolean indexNeedsBeLogged(int logRequestNumber, int logMultiple) {
+        return (logRequestNumber >= logMultiple) && (logRequestNumber % logMultiple == 0);
+    }
+}
