ddde4269e66c16275e2199d850e07454ad685781
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:14:37 +0200

    [~] - StopTransactionRequest was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/StopTransactionRequest.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/StopTransactionRequest.java
@@ -1,15 +1,26 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model.payload;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@RequiredArgsConstructor
+import static com.extrawest.ocpp.emulator.chargepoint.cli.constant.ModelConstants.DATE_FORMAT;
+
+@RequiredArgsConstructor(onConstructor = @__(@JsonCreator))
+@Getter
 public class StopTransactionRequest {
 
+    @JsonProperty("meterStop")
     private final int meterStop;
 
+    @JsonFormat(pattern = DATE_FORMAT)
+    @JsonProperty("timestamp")
     private final LocalDateTime timestamp;
 
+    @JsonProperty("transactionId")
     private final int transactionId;
 }
