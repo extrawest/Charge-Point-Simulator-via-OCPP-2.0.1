9f14825d016a9ae747e45378353aae3ca66d06cb
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:12:02 +0200

    [~] - HeartbeatConfirmation was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/HeartbeatConfirmation.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/HeartbeatConfirmation.java
@@ -4,20 +4,17 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 import static com.extrawest.ocpp.emulator.chargepoint.cli.constant.ModelConstants.DATE_FORMAT;
 
 @Getter
+@RequiredArgsConstructor(onConstructor = @__(@JsonCreator))
 public class HeartbeatConfirmation {
 
+    @JsonProperty("currentTime")
+    @JsonFormat(pattern = DATE_FORMAT)
     private final LocalDateTime currentTime;
-
-    @JsonCreator
-    public HeartbeatConfirmation(
-        @JsonProperty("currentTime") @JsonFormat(pattern = DATE_FORMAT) LocalDateTime currentTime
-    ) {
-        this.currentTime = currentTime;
-    }
 }
