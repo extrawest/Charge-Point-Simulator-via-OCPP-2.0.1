ee5ab332d89f0fb64a852858f931525c23f6d637
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:13:27 +0200

    [~] - StartTransactionRequest was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/StartTransactionRequest.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/StartTransactionRequest.java
@@ -1,14 +1,19 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model.payload;
 
 import com.extrawest.ocpp.emulator.chargepoint.cli.model.IdToken;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+import static com.extrawest.ocpp.emulator.chargepoint.cli.constant.ModelConstants.DATE_FORMAT;
+
 @RequiredArgsConstructor
 @ToString
+@Getter
 public class StartTransactionRequest {
 
     private final int connectorId;
@@ -18,5 +23,6 @@ public class StartTransactionRequest {
 
     private final int meterStart;
 
+    @JsonFormat(pattern = DATE_FORMAT)
     private final LocalDateTime timestamp;
 }
