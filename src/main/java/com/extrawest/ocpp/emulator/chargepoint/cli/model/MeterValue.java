84c8efa21e6b6d95c7d4a623cae533343e222510
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:12:45 +0200

    [~] - MeterValue was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/MeterValue.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/MeterValue.java
@@ -1,18 +1,26 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model;
 
 import com.extrawest.ocpp.emulator.chargepoint.cli.exception.IllegalArgumentApplicationException;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Singular;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.extrawest.ocpp.emulator.chargepoint.cli.constant.ModelConstants.DATE_FORMAT;
+
 @Builder
+@Getter
 public class MeterValue {
 
+    @JsonFormat(pattern = DATE_FORMAT)
     private final LocalDateTime timestamp;
 
     @Singular
+    @JsonProperty("sampledValue")
     private final List<SampledValue> sampledValues;
 
     public MeterValue(LocalDateTime timestamp, List<SampledValue> sampledValues) {
