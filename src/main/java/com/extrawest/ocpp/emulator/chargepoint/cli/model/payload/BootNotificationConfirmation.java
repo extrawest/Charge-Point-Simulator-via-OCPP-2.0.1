73e2e827e7beb805e25ffaae5cee5e0cddf65834
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:11:50 +0200

    [~] - BootNotificationConfirmation was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/BootNotificationConfirmation.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/payload/BootNotificationConfirmation.java
@@ -5,28 +5,23 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 import static com.extrawest.ocpp.emulator.chargepoint.cli.constant.ModelConstants.DATE_FORMAT;
 
 @Getter
+@RequiredArgsConstructor(onConstructor = @__(@JsonCreator))
 public class BootNotificationConfirmation {
 
+    @JsonProperty("currentTime")
+    @JsonFormat(pattern = DATE_FORMAT)
     private final LocalDateTime currentTime;
 
+    @JsonProperty("interval")
     private final int interval;
 
+    @JsonProperty("status")
     private final RegistrationStatus status;
-
-    @JsonCreator
-    public BootNotificationConfirmation(
-        @JsonProperty("currentTime") @JsonFormat(pattern = DATE_FORMAT) LocalDateTime currentTime,
-        @JsonProperty("interval") int interval,
-        @JsonProperty("status") RegistrationStatus status
-    ) {
-        this.currentTime = currentTime;
-        this.interval = interval;
-        this.status = status;
-    }
 }
