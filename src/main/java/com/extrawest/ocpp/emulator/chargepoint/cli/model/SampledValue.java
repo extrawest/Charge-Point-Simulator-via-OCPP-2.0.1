5eae719db4c7d9abc481482923694287dd62f2b9
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:13:06 +0200

    [~] - SampledValue was annotated for correct JSON serialization

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/SampledValue.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/SampledValue.java
@@ -1,9 +1,11 @@
 package com.extrawest.ocpp.emulator.chargepoint.cli.model;
 
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public class SampledValue {
 
     @NonNull
