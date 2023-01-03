bff36bc74171137ee4a125c74e3a026b76940a56
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-03 23:32:33 +0200

    [+] - SEC_WEBSOCKET_PROTOCOL_HEADER_NAME, SEC_WEBSOCKET_PROTOCOL_HEADER_VALUE were added

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/constant/ModelConstants.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/constant/ModelConstants.java
@@ -9,4 +9,8 @@ public class ModelConstants {
     public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     public static final int DEFAULT_CONNECTOR_ID = 1;
+
+    public static final String SEC_WEBSOCKET_PROTOCOL_HEADER_NAME = "Sec-WebSocket-Protocol";
+
+    public static final String SEC_WEBSOCKET_PROTOCOL_HEADER_VALUE = "ocpp1.6";
 }
