eeaa39c297c2b830544537d02ed660e0fd3a920a
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-11 13:53:39 +0200

    [~] - injecting the bean of WebsocketEventListener into JettyWebsocketClient instances

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/impl/ChargePointEmulatorFactoryImpl.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/impl/ChargePointEmulatorFactoryImpl.java
@@ -3,6 +3,7 @@ package com.extrawest.ocpp.emulator.chargepoint.cli.emulator.impl;
 import com.extrawest.ocpp.emulator.chargepoint.cli.dto.CreateChargePointParameters;
 import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulator;
 import com.extrawest.ocpp.emulator.chargepoint.cli.emulator.ChargePointEmulatorFactory;
+import com.extrawest.ocpp.emulator.chargepoint.cli.event.WebsocketEventListener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
@@ -22,6 +24,8 @@ public class ChargePointEmulatorFactoryImpl implements ChargePointEmulatorFactor
     private final WebSocketClient webSocketClient;
 
     private final ClientUpgradeRequest clientUpgradeRequest;
+
+    private final WebsocketEventListener websocketEventListener;
 
     @Value("${ocpp.charge-point.meter-values.send-interval:PT10S}")
     @NonNull
@@ -36,12 +40,15 @@ public class ChargePointEmulatorFactoryImpl implements ChargePointEmulatorFactor
     @Override
     public ChargePointEmulator createChargePointEmulator(CreateChargePointParameters createChargePointParameters) {
         return ChargePointEmulator.builder()
-            .centralSystemClient(new JettyWebsocketClient(objectMapper, webSocketClient, clientUpgradeRequest))
+            .centralSystemClient(
+                new JettyWebsocketClient(objectMapper, webSocketClient, clientUpgradeRequest, websocketEventListener)
+            )
             .chargePointModel(chargePointModel)
             .chargePointVendor(chargePointVendor)
             .chargePointId(createChargePointParameters.getChargePointId())
             .centralSystemUrl(createChargePointParameters.getCentralSystemUrl())
             .sendMeterValuesInterval(sendMeterValuesInterval)
+            .currentMeterValue(new AtomicInteger(0))
             .build();
     }
 }
