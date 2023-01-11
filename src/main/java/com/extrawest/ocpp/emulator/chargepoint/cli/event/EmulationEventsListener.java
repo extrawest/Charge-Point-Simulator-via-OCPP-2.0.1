124d248c9fb964bd6225cdd36a53fb93ceef53ce
Mykhailo Birintsev <mykhailo.birintsev@extrawest.com>
2023-01-11 13:32:45 +0200

    [+] - EmulationEventsListener was added

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/event/EmulationEventsListener.java
@@ -0,0 +1,14 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.event;
+
+public interface EmulationEventsListener {
+
+    void onBootNotificationSent();
+
+    void onHeartbeatSent();
+
+    void onAuthorizeSent();
+
+    void onStartTransactionSent();
+
+    void onMeterValuesSent();
+}
