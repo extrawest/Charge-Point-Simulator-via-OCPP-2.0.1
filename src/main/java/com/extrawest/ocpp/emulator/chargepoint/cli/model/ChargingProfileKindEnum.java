da7e4cc2e37a9a0fb649cca790e64a46c9ab3485
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 17:00:43 +0200

    added ChargingProfileKindEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/ChargingProfileKindEnum.java
@@ -0,0 +1,54 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Charging_ Profile. Charging_ Profile_ Kind. Charging_ Profile_ Kind_ Code
+ * urn:x-oca:ocpp:uid:1:569232
+ * Indicates the kind of schedule.
+ *
+ *
+ */
+public enum ChargingProfileKindEnum {
+
+    ABSOLUTE("Absolute"),
+    RECURRING("Recurring"),
+    RELATIVE("Relative");
+    private final String value;
+    private final static Map<String, ChargingProfileKindEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (ChargingProfileKindEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    ChargingProfileKindEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ChargingProfileKindEnum fromValue(String value) {
+        ChargingProfileKindEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
