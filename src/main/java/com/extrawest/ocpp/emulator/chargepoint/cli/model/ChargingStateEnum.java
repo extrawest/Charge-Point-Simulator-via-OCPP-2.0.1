03695e3b8046d3906a6f006ddb35312b23452083
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 17:00:12 +0200

    added ChargingStateEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/ChargingStateEnum.java
@@ -0,0 +1,57 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Transaction. State. Transaction_ State_ Code
+ * urn:x-oca:ocpp:uid:1:569419
+ * Current charging state, is required when state
+ * has changed.
+ *
+ *
+ */
+public enum ChargingStateEnum {
+
+    CHARGING("Charging"),
+    EV_CONNECTED("EVConnected"),
+    SUSPENDED_EV("SuspendedEV"),
+    SUSPENDED_EVSE("SuspendedEVSE"),
+    IDLE("Idle");
+    private final String value;
+    private final static Map<String, ChargingStateEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (ChargingStateEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    ChargingStateEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ChargingStateEnum fromValue(String value) {
+        ChargingStateEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
