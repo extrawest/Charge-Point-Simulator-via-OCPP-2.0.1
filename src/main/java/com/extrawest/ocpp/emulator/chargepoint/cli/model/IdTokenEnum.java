97bde55d0733c55b29348fe1387c5ef48318ebbc
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:59:26 +0200

    added IdTokenEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/IdTokenEnum.java
@@ -0,0 +1,57 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Enumeration of possible idToken types.
+ *
+ *
+ */
+public enum IdTokenEnum {
+
+    CENTRAL("Central"),
+    E_MAID("eMAID"),
+    ISO_14443("ISO14443"),
+    ISO_15693("ISO15693"),
+    KEY_CODE("KeyCode"),
+    LOCAL("Local"),
+    MAC_ADDRESS("MacAddress"),
+    NO_AUTHORIZATION("NoAuthorization");
+    private final String value;
+    private final static Map<String, IdTokenEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (IdTokenEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    IdTokenEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static IdTokenEnum fromValue(String value) {
+        IdTokenEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
