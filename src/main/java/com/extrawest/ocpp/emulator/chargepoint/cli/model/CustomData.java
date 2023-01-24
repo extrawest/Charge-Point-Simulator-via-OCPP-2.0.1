01cfae17af78d991cb12d2ea8402e69484b1933c
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:59:40 +0200

    added CustomData.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/CustomData.java
@@ -0,0 +1,58 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Builder;
+import lombok.Getter;
+
+
+/**
+ * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "vendorId"
+})
+@Builder
+@Getter
+public class CustomData {
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("vendorId")
+    public String vendorId;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.vendorId == null)? 0 :this.vendorId.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof CustomData rhs)) {
+            return false;
+        }
+        return ((Objects.equals(this.vendorId, rhs.vendorId))&&(Objects.equals(this.additionalProperties, rhs.additionalProperties)));
+    }
+
+}
