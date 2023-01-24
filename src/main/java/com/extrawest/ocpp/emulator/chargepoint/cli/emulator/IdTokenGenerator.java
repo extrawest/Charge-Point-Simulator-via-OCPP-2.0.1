e833abb427dbd24357520fd683265c6ed723f2bc
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 17:03:30 +0200

    removed IdTokenGenerator

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/emulator/IdTokenGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.extrawest.ocpp.emulator.chargepoint.cli.emulator;
-
-import com.extrawest.ocpp.emulator.chargepoint.cli.model.IdToken;
-
-public interface IdTokenGenerator {
-
-    IdToken generateRandomToken();
-}
