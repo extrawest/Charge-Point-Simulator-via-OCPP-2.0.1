c0295cc4f044be3f046994151c168a20d9ae1f36
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:59:12 +0200

    added LocationEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/LocationEnum.java
@@ -0,0 +1,57 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Sampled_ Value. Location. Location_ Code
+ * urn:x-oca:ocpp:uid:1:569265
+ * Indicates where the measured value has been sampled. Default = "Outlet"
+ *
+ *
+ *
+ */
+public enum LocationEnum {
+
+    BODY("Body"),
+    CABLE("Cable"),
+    EV("EV"),
+    INLET("Inlet"),
+    OUTLET("Outlet");
+    private final String value;
+    private final static Map<String, LocationEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (LocationEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    LocationEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static LocationEnum fromValue(String value) {
+        LocationEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
