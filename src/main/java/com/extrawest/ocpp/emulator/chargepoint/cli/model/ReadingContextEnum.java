b5e56e44f6c3d3b7d2fe92799c5df3fe6b1dfa0c
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:58:39 +0200

    added ReadingContextEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/ReadingContextEnum.java
@@ -0,0 +1,59 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Sampled_ Value. Context. Reading_ Context_ Code
+ * urn:x-oca:ocpp:uid:1:569261
+ * Type of detail value: start, end or sample. Default = "Sample.Periodic"
+ *
+ *
+ */
+public enum ReadingContextEnum {
+
+    INTERRUPTION_BEGIN("Interruption.Begin"),
+    INTERRUPTION_END("Interruption.End"),
+    OTHER("Other"),
+    SAMPLE_CLOCK("Sample.Clock"),
+    SAMPLE_PERIODIC("Sample.Periodic"),
+    TRANSACTION_BEGIN("Transaction.Begin"),
+    TRANSACTION_END("Transaction.End"),
+    TRIGGER("Trigger");
+    private final String value;
+    private final static Map<String, ReadingContextEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (ReadingContextEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    ReadingContextEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static ReadingContextEnum fromValue(String value) {
+        ReadingContextEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
