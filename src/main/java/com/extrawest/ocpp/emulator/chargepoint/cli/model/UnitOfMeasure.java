c23ab6d67bd6e1cbef41557b9e70c7db6c93d2cf
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:57:14 +0200

    added UnitOfMeasure.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/UnitOfMeasure.java
@@ -0,0 +1,69 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Objects;
+
+
+/**
+ * Represents a UnitOfMeasure with a multiplier
+ *
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "customData",
+        "unit",
+        "multiplier"
+})
+@Builder
+@Getter
+public class UnitOfMeasure {
+
+    /**
+     * This class does not get 'AdditionalProperties = false' in the schema generation, so it can be extended with arbitrary JSON properties to allow adding custom data.
+     *
+     */
+    @JsonProperty("customData")
+    public CustomData customData;
+    /**
+     * Unit of the value. Default = "Wh" if the (default) measurand is an "Energy" type.
+     * This field SHALL use a value from the list Standardized Units of Measurements in Part 2 Appendices.
+     * If an applicable unit is available in that list, otherwise a "custom" unit might be used.
+     *
+     *
+     */
+    @JsonProperty("unit")
+    public String unit = "Wh";
+    /**
+     * Multiplier, this value represents the exponent to base 10. I.e. multiplier 3 means 10 raised to the 3rd power. Default is 0.
+     *
+     *
+     */
+    @JsonProperty("multiplier")
+    public Integer multiplier = 0;
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.customData == null)? 0 :this.customData.hashCode()));
+        result = ((result* 31)+((this.unit == null)? 0 :this.unit.hashCode()));
+        result = ((result* 31)+((this.multiplier == null)? 0 :this.multiplier.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof UnitOfMeasure rhs)) {
+            return false;
+        }
+        return (((Objects.equals(this.customData, rhs.customData))&&(Objects.equals(this.unit, rhs.unit)))&&(Objects.equals(this.multiplier, rhs.multiplier)));
+    }
+}
