52808a240fb8afc81115b9971a41fd4b895f5f90
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:58:16 +0200

    added RequestStartStopStatusEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/RequestStartStopStatusEnum.java
@@ -0,0 +1,51 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Status indicating whether the Charging Station accepts the request to start a transaction.
+ *
+ *
+ */
+public enum RequestStartStopStatusEnum {
+
+    ACCEPTED("Accepted"),
+    REJECTED("Rejected");
+    private final String value;
+    private final static Map<String, RequestStartStopStatusEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (RequestStartStopStatusEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    RequestStartStopStatusEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static RequestStartStopStatusEnum fromValue(String value) {
+        RequestStartStopStatusEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
