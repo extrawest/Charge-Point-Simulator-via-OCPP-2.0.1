7fb414865eaf6e0228f2846a8d6a2b37e1c3990f
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:58:27 +0200

    added RecurrencyKindEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/RecurrencyKindEnum.java
@@ -0,0 +1,53 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Charging_ Profile. Recurrency_ Kind. Recurrency_ Kind_ Code
+ * urn:x-oca:ocpp:uid:1:569233
+ * Indicates the start point of a recurrence.
+ *
+ *
+ */
+public enum RecurrencyKindEnum {
+
+    DAILY("Daily"),
+    WEEKLY("Weekly");
+    private final String value;
+    private final static Map<String, RecurrencyKindEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (RecurrencyKindEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    RecurrencyKindEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static RecurrencyKindEnum fromValue(String value) {
+        RecurrencyKindEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
