85cbecb21057c723763ac4647b3079487cc09703
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-24 16:57:32 +0200

    added TransactionEventEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/TransactionEventEnum.java
@@ -0,0 +1,53 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * This contains the type of this event.
+ * The first TransactionEvent of a transaction SHALL contain: "Started" The last TransactionEvent of a transaction SHALL contain: "Ended" All others SHALL contain: "Updated"
+ *
+ *
+ */
+public enum TransactionEventEnum {
+
+    ENDED("Ended"),
+    STARTED("Started"),
+    UPDATED("Updated");
+    private final String value;
+    private final static Map<String, TransactionEventEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (TransactionEventEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    TransactionEventEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static TransactionEventEnum fromValue(String value) {
+        TransactionEventEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
