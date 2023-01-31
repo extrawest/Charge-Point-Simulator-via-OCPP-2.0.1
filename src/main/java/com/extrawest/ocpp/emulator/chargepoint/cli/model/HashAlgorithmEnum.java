110830722ee8e5bff19703f9d8d040283cb023b9
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-01-31 12:58:14 +0200

    added HashAlgorithmEnum.java

---
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/HashAlgorithmEnum.java
@@ -0,0 +1,52 @@
+package com.extrawest.ocpp.emulator.chargepoint.cli.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+
+/**
+ * Used algorithms for the hashes provided.
+ *
+ *
+ */
+public enum HashAlgorithmEnum {
+
+    SHA_256("SHA256"),
+    SHA_384("SHA384"),
+    SHA_512("SHA512");
+    private final String value;
+    private final static Map<String, HashAlgorithmEnum> CONSTANTS = new HashMap<>();
+
+    static {
+        for (HashAlgorithmEnum c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    HashAlgorithmEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @JsonValue
+    public String value() {
+        return this.value;
+    }
+
+    @JsonCreator
+    public static HashAlgorithmEnum fromValue(String value) {
+        HashAlgorithmEnum constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
+
+}
