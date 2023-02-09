02458cfe742cf282b4a49ffcf868d36859b983d2
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2023-02-09 17:23:02 +0200

    removed using request start\stop transaction actions

--- src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/impl/CallFactoryImpl.java
+++ src/main/java/com/extrawest/ocpp/emulator/chargepoint/cli/model/call/impl/CallFactoryImpl.java
@@ -31,16 +31,6 @@ public class CallFactoryImpl implements CallFactory {
         return new Call<>(generateUniqueId(), CallAction.TransactionEvent, transactionEventRequest);
     }
 
-    @Override
-    public Call<RequestStartTransactionRequest> createCallFor(RequestStartTransactionRequest requestStartTransactionRequest) {
-        return new Call<>(generateUniqueId(), CallAction.RequestStartTransaction, requestStartTransactionRequest);
-    }
-
-    @Override
-    public Call<RequestStopTransactionRequest> createCallFor(RequestStopTransactionRequest requestStopTransactionRequest) {
-        return new Call<>(generateUniqueId(), CallAction.RequestStopTransaction, requestStopTransactionRequest);
-    }
-
     private String generateUniqueId() {
         return UUID.randomUUID().toString();
     }
